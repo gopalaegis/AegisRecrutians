862e1aa0ffc8aa8b76a40cc5b3a972f72b5f9c30
Deep Patel <developers@shankhitech.com>
2021-08-05 15:08:04 +0530

    commit all

--- CONSTRUCTION/Controllers/jobsController.cs
+++ CONSTRUCTION/Controllers/jobsController.cs
@@ -64,6 +64,7 @@ namespace CONSTRUCTION.Controllers
             }
             m = m.Where(x => x.City.ToLower().Contains(city.ToLower()) && x.Title.ToLower().Contains(title.ToLower())).ToList();
             model.currentPage = page;
+            model.totalRecord = m.Count;
             var pageCount = (double)m.Count / (double)5;
             model.totalPage = (int)Math.Ceiling(pageCount);
             var skip = ((page - 1) * 5);
@@ -78,13 +79,13 @@ namespace CONSTRUCTION.Controllers
             }
             if (model.jobDetailViewModels.Count > 0)
             {
-                model.fromtojob = (skip + 1).ToString() + " - " + (skip + model.jobDetailViewModels.Count) + " of " + model.totalPage + " Jobs";
+                model.fromtojob = (skip + 1).ToString() + " - " + (skip + model.jobDetailViewModels.Count) + " of " + model.totalRecord + " Jobs";
             }
             else
             {
                 model.fromtojob = "0 - 0 of 0 Jobs";
             }
-            return View("_partialJobList", model);
+            return PartialView("_partialJobList", model);
         }
     }
 }
